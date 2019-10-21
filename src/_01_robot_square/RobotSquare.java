2cbfd8fc5f061dcdfc30dc35794270e35fa918fe
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-21 09:55:41 -0700

    started RobotSquare

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,7 +12,7 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+    	Robot bob = new Robot();
 
         // 3. Put the robot's pen down
 
